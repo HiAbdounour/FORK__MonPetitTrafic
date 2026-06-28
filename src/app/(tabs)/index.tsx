ada7a5c3ebdf3ebe465224374539b3a5d3e2e0ea
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-28 19:29:16 +0200

    Build the header of the home page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
-import { View,Text } from 'react-native';
+import { View,Text,Image } from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import './index.css';
 
 function Home(){
 
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className='bodyview'>
+
+            <View className="header">
+                <View className='logoview'>
+                    <Image
+                        source={require('../../assets/images/logo.png')}
+                        className='home_logo'
+                    />
+                </View>
+                <Text className='version_label'>
+                    v0.0.1a
+                </Text>
+            </View>
+            
+        </SafeAreaView>
     )
 }
+
+export default Home
