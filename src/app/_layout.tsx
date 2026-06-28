3ee19ed97e6df850db7be0aa49bddb3e033d5127
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-28 20:32:04 +0200

    Make a simple layout without default header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,1 +1,9 @@
-// empty
+import { Stack } from "expo-router";
+import '@/global.css';
+
+//SplashScreen.preventAutoHideAsync() when loading
+//SplashScreen.hideAsync() when finish loading
+
+export default function RootLayout() {
+  return <Stack screenOptions={{headerShown:false}}/>;
+}
