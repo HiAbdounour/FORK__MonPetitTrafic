9a8d70e9a2544db07d031dc56cc1fc02fa8eaaba
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-30 19:44:26 +0200

    Custom Toggle styles

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -3,7 +3,41 @@ import { Pressable,View,StyleSheet } from 'react-native'
 
 function StyleToggle(){
     return StyleSheet.create({
+        toggle:{
+            borderWidth: 5,
+            borderRadius: '9999px',
+            borderColor: 'grey',
+            width: 100,
+            height: 40,
+            marginLeft: 10
+        },
 
+        boxOff:{
+            backgroundColor: 'grey'
+        },
+        boxOn:{
+            backgroundColor: 'lightgreen'
+        },
+
+
+        toggleCircle:{
+            borderWidth: 5,
+            borderRadius: '9999px',
+            width: 32,
+            height: 32
+        },
+
+        circleOff:{
+            margin: 0,
+            backgroundColor:'white',
+            borderColor: 'black'
+        },
+        circleOn:{
+            margin: 0,
+            marginLeft: 60,
+            backgroundColor: 'white',
+            borderColor: 'grey'
+        }
     })
 }
 
@@ -16,11 +50,9 @@ function Toggle(){
     }
 
     return(
-        <Pressable
-            onPress={handleSwitch}
-        >
-            <View>
-                <View></View>
+        <Pressable onPress={handleSwitch}>
+            <View style={isActive ? [styles.toggle,styles.boxOn] : [styles.toggle,styles.boxOff]}>
+                <View style={isActive ? [styles.toggleCircle,styles.circleOn] : [styles.toggleCircle,styles.circleOff]}></View>
             </View>
         </Pressable>
     )
